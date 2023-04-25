8faaa484af9316498552041ecd916907d3033522
sjamescarter <stephenjamescarter@gmail.com>
2023-04-25 15:37:53 -0500

    write migrations

--- db/migrate/20230425120433_create_comments.rb
+++ db/migrate/20230425120433_create_comments.rb
@@ -1,4 +1,10 @@
-class Comments < ActiveRecord::Migration[6.1]
+class CreateComments < ActiveRecord::Migration[6.1]
   def change
+    create_table :comments do |t|
+      t.string :name
+      t.text :comment
+      t.integer :post_id
+      t.timestamps
+    end
   end
 end
